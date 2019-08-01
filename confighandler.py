04d6db9e616d2c68bdbf6f2e689d6035040dd795
Alex <tiamatoru@gmail.com>
2019-08-01 16:28:37 +0300

    ready

--- confighandler.py
+++ confighandler.py
@@ -43,23 +43,15 @@ def create_config():
         'Production':create_config_unit(),
         'Develop':create_config_unit()
     }
-
+    return config
 
 def write_json(person_dict):# Writes a list as the argument into the file. If file doesn't exist it will be created.
     try:
-        data=json.load(open('persons.json'))
+        data=json.load(open('config.json'))
     except:
-        data=[]
-    data.append(person_dict)
-
-    with open('persons.json', 'w') as file:
-        json.dump(data, file, indent=2, ensure_ascii=False)
-
-
-
-
-
-
+        data=create_config()
+        with open('config.json', 'w') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
 
 
 
