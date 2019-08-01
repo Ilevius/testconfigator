feed3893ba1c21a3de3d186e94561af108376194
Alex <tiamatoru@gmail.com>
2019-08-01 13:41:20 +0300

    1st

---
+++ confighandler.py
@@ -0,0 +1,28 @@
+import json
+from random import choice
+
+
+def gen_person():
+    name=''
+    tel=''
+    letters=['a','b','c']
+    nums=['1','2','3']
+
+    while len(name) !=5
+       name+=choice(letters)
+       tel+=choice(nums)
+
+
+
+
+
+
+
+
+def main():
+    gen_person()
+
+
+
+if __name__=='__main__':
+    main()
