df9145c29931b458450616fbef5033dbc1e37253
Alex <tiamatoru@gmail.com>
2019-08-02 21:17:59 +0300

    class in class

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from lib.config import Config
+import json
 
 if __name__ == '__main__':
     print('Run!')
     conf = Config()
     print(conf.get_json_string())
 
+    print('\n ******Next step*****!\n')
+    configfile=open("config.json", mode='r')
+    configcontent=json.load(configfile)
+    print(configcontent)
