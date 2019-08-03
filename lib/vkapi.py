03a9ef0d35f7932f11f93dbb17c705b541b3165a
Alex <tiamatoru@gmail.com>
2019-08-03 12:20:50 +0300

    seemstobedone

--- lib/vkapi.py
+++ lib/vkapi.py
@@ -3,12 +3,9 @@ from .iUnit import IUnit
 class VK_API(IUnit):
     def __init__(self,unitkind,source_convey_layer3):
         self.token=source_convey_layer3[unitkind]['vkapi']['token']
-
-        pass
-    secret_key='secret_key'
-    auth_token='auth_token'
-    group_id='group_id'
-
+        self.secret_key=source_convey_layer3[unitkind]['vkapi']['secret-key']
+        self.auth_token=source_convey_layer3[unitkind]['vkapi']['auth-token']
+        self.group_id=source_convey_layer3[unitkind]['vkapi']['group-id']
     def get_json(self):
         return{
                 "token":self.token,
