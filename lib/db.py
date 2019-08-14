5da4230b0ac49863440400cc687e3065ec2c2e20
Alex <tiamatoru@gmail.com>
2019-08-14 09:16:03 +0300

    New comments

--- lib/db.py
+++ lib/db.py
@@ -1,3 +1,8 @@
+'''
+    Экземпляры сего класса при создании пишут в своё поле данные из объекта.
+    Ссылка на объект собирается из аргуменьтов, принятых при создании
+    экземпляра.
+'''
 class DB():
     def __init__(self,unitkind,source_convey_layer3):
         self.connection_string = source_convey_layer3[unitkind]['database']['connectionString']
--- lib/vkapi.py
+++ lib/vkapi.py
@@ -1,3 +1,8 @@
+'''
+    Экземпляры сего класса при создании пишут в свои поля данные из объекта.
+    Ссылка на объект собирается из аргуменьтов, принятых при создании
+    экземпляра.
+'''
 class VK_API():
     def __init__(self,unitkind,source_convey_layer3):
         self.token=source_convey_layer3[unitkind]['vkapi']['token']
